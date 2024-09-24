172ac1b963bef32c77552cb5bbea6b8c873ce348
NitikaPrithwani <nitikaprithwani55@gmail.com>
2024-09-24 15:30:33 +0530

    Deploy EduLearning

--- app/modules/Home/Testimonials/TestimonialCard.jsx
+++ app/modules/Home/Testimonials/TestimonialCard.jsx
@@ -1,6 +1,6 @@
 import { testimonials } from "@/static-data/TestimonialsData";
 
-export default function TestimonialCard({currentSlide}) {
+export default function TestimonialCard({ currentSlide }) {
     return (
         <>
             <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-center">
@@ -13,8 +13,8 @@ export default function TestimonialCard({currentSlide}) {
                         <div className="flex-shrink-0 w-full md:w-auto">
                             <div className="w-full md:w-96 md:h-96 bg-cover bg-center rounded-lg shadow-lg" style={{ backgroundImage: `url(${testimonial.imageSrc})` }} />
                         </div>
-                        <div  className="bg-white text-gray-600 marginLeft rounded-xl shadow-lg p-6 md:p-8 w-full md:w-7/12 mt-6 md:mt-0">
-                            <p className="italic mb-4 text-lg md:text-xl tracking-wide">"{testimonial.quote}"</p>
+                        <div className="bg-white text-gray-600 marginLeft rounded-xl shadow-lg p-6 md:p-8 w-full md:w-7/12 mt-6 md:mt-0">
+                            <p className="italic mb-4 text-lg md:text-xl tracking-wide">&quot;{testimonial.quote}&quot;</p>
                             <h4 className="text-xl md:text-3xl tracking-wider text-gray-900">{testimonial.name}</h4>
                             <p className="text-blue-500 text-base md:text-base">{testimonial.title}</p>
                         </div>
@@ -22,5 +22,5 @@ export default function TestimonialCard({currentSlide}) {
                 ))}
             </div>
         </>
-    )
+    );
 }
